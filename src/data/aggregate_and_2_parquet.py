8b2e6ef9adef69e43bf97cb065e7219db5c4bca4
Jaime A. Campos V <jcampos@posteo.net>
2021-04-24 20:30:12 +0200

    update scr

--- src/data/aggregate_and_2_parquet.py
+++ src/data/aggregate_and_2_parquet.py
@@ -14,18 +14,20 @@ args = parser.parse_args()
 input = Path(args.input_dir)
 output = Path(args.output_dir)
 
-list_files = [x.resolve() for x in input.glob('**/*.txt')]
-chunks = np.array_split(list_files, 10)
+# Decompress
+list_gz = [x.resolve() for x in input.glob('**/*.gz')]
+
+chunks = np.array_split(list_gz, 10)
 for ix, chunk in enumerate(chunks):
-    output_filename = output / f"chunk_{ix}.parquet"
+    output_filename = output / "interim" /f"chunk_{ix}.parquet"
     print(output_filename.resolve())
     tmp_df = pd.concat(
-        (pd.read_table(filename, delimiter='\t', header=None, index_col=0).transpose() for filename in chunk),
+        (pd.read_table(filename, delimiter='\t', header=None, index_col=0, compression='gzip').transpose() for filename in chunk),
         axis=0)
     tmp_df.set_index(pd.Series([Path(x).with_suffix('').stem for  x in chunk]), inplace=True)
     tmp_df.to_parquet(output_filename.resolve())
     del tmp_df
 
-list_chunks = [x.resolve() for x in output.glob('**/*.parquet')]
+list_chunks = [x.resolve() for x in output.glob('interim/*.parquet')]
 final = pd.concat((pd.read_parquet(chunk) for chunk in list_chunks), axis=0)
-final.to_parquet((output / "processed/complete_dataset.parquet").resolve())
+final.to_parquet((output / "processed"/ "complete_dataset.parquet").resolve())
